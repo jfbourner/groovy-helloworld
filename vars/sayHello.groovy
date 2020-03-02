e873a72c484cc933ae4c9ee955cd7456826fc0ef
jack <jack>
2020-03-02 21:13:36 +0000

    .

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,21 @@
 def call(String name = 'human') {
     echo "Hello, ${name}."
+    def a = "hello";
+    String jack = "jack"
+    println("error")
+
+    def prop = loadFile();
+    def port = prop.getProperty("server.port")
+    println(port)
+
+}
+
+def loadFile() {
+
+    new File('./target/classes/application.properties').withReader {
+        def props = new Properties()
+        props.load(it)
+        return props;
+    }
+
 }
