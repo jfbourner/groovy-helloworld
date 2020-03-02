e8674dbff404610f3054cfe630726db001ebd31a
jack <jack>
2020-03-02 22:50:57 +0000

    .

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,15 +2,13 @@ def call(String name = 'human') {
     echo "Hello, ${name}."
 
     def prop = loadFile();
-    def port = prop.getProperty("server.port")
+    def port = prop['server.port']
     println(port)
 
 }
 
 def loadFile() {
-    def workspace = env.WORKSPACE
-    Properties properties = new Properties()
-    def propFileContent = readFile "${workspace}/target/classes/application.properties"
+    def props = readProperties file: 'target/classes/application.properties'
     properties.load(propFileContent)
     return properties;
 }
